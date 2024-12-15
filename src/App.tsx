68572a7ae6f770f5c4bfd4aaa9d7d3305c09c6e3
Aldi Saputra <aldimks7464@gmail.com>
2024-12-16 04:23:42 +0800

    For one Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomeView from './Views/HomeView';
-import ProjectDetailView from './Views/ProjectDetailView';
 import UniversityProject from './components/Detail/UniversityProject';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route path="/project" element={<ProjectDetailView />} />
-        <Route path="/university" element={<UniversityProject />} />
+        <Route path="/" element={<UniversityProject />} />
       </Routes>
     </Router>
   );
